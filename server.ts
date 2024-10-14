eb4024bfd0be1022d685a303993d2c8c1efa237e
VishnuSusilkumar <vishnususilkumar10@gmail.com>
2024-10-14 10:31:21 +0530

    first commit

---
+++ server.ts
@@ -0,0 +1,36 @@
+import express from 'express';
+import cors from 'cors';
+import dotenv from 'dotenv';
+import cookieParser from 'cookie-parser';
+import router from './src/routes/authRoute';
+import connectDB from './src/config/db';
+import imageRouter from './src/routes/imageRoute';
+
+dotenv.config();
+
+const app = express();
+
+
+connectDB()
+    .then(() => console.log('Connected to Database'))
+    .catch(err => {
+        console.error('Database connection failed:', err);
+        process.exit(1); 
+    });
+
+app.use(express.json());
+app.use(cors({
+    origin: "https://stock-image-frontend.vercel.app",
+    methods: ["GET", "POST", "PUT", "PATCH", "DELETE"],
+    credentials: true
+}));
+app.use(cookieParser());
+
+app.use('/api/auth', router);
+app.use('/api/image', imageRouter);
+
+const port = process.env.PORT || 10000;
+
+app.listen(port, () => {
+    console.log(`Server running on PORT: ${port}`);
+});
---
+++ src/routes/authRoute.ts
@@ -0,0 +1,28 @@
+import express, { Router } from 'express';
+import { register, activateUser, login, refreshToken, logout, profileDetails, forgotPassword, verifyOTP, resetPassword, hello } from '../controller/authController';
+import { isvalidate } from '../middleware/authMiddleware';
+
+
+const router: Router = express.Router();
+
+router.post('/register', register);
+
+router.post('/activate', activateUser);
+
+router.post('/login', login);
+
+router.post('/refresh-token', refreshToken);
+
+router.post('/logout', logout);
+
+router.post('/profile',isvalidate,profileDetails)
+
+router.post('/forgot-password',forgotPassword);
+
+router.post('/verify-otp',verifyOTP);
+
+router.post('/reset-password',resetPassword);
+
+router.get('/hello',hello);
+
+export default router;
